1477e8893cc4e7889df85bac9df9a92252d31d72
Yaren Su <atasu07@hotmail.com>
2024-04-04 23:49:55 +0300

    Category için Update ve Delete işlemleri yapıldı

--- MyLinq/Controllers/CategoryController.cs
+++ MyLinq/Controllers/CategoryController.cs
@@ -9,6 +9,7 @@ namespace MyLinq.Controllers
 {
     public class CategoryController : Controller
     {
+        #region CategoryList
         // GET: Category
         public ActionResult CategoryIndex()
         {
@@ -18,8 +19,10 @@ namespace MyLinq.Controllers
 
             return View(list);
         }
-        [HttpGet]
+        #endregion
 
+
+        [HttpGet]
         public ActionResult CategoryUpdate(int id)
         {
             NORTHWNDEntities db = new NORTHWNDEntities();
@@ -32,17 +35,80 @@ namespace MyLinq.Controllers
         [HttpPost]//post işlemleri; edit,remove,insert...
         public ActionResult CategoryUpdate(Categories categories)
         {
-            NORTHWNDEntities db = new NORTHWNDEntities();
-            var getirCategory = db.Categories.Where(p => p.CategoryID ==categories.CategoryID).FirstOrDefault();
-            getirCategory.CategoryName=categories.CategoryName;
+            try
+            {
+                NORTHWNDEntities db = new NORTHWNDEntities();
+                var getirCategory = db.Categories.Where(p => p.CategoryID == categories.CategoryID).FirstOrDefault();
+                getirCategory.CategoryName = categories.CategoryName;
+                getirCategory.Picture = getirCategory.Picture;
+                getirCategory.Description = categories.Description;
 
-            getirCategory.Description = categories.Description;
+                int saveResult = db.SaveChanges();
+                //bavereges-deneme
+                if (saveResult > 0)
+                {
+                    ViewBag.saveMessage = "Kategori başarılı bir şekilde güncellendi";
+                    return View();
+                }
+                else
+                {
+                    ViewBag.saveMessage = "Kategori güncelleme işlemi başarısız oldu";
+                    ViewData["mesaj"] = "deneme";
+                    TempData["enBuyukData"] = getirCategory;
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.saveMessage = "Kategori güncelleme esnasında bir hata oluştu\nHATA:" + ex.Message;
+
+                return View();
+            }
 
 
-            db.SaveChanges();
-            return View();
 
         }
 
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var getirCategory = db.Categories.Where(p => p.CategoryID == id).FirstOrDefault();
+
+            return View(getirCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Categories categories)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var getirCategory = db.Categories.Where(p => p.CategoryID ==categories.CategoryID).FirstOrDefault();
+             db.Categories.Remove(getirCategory);
+
+            int result = db.SaveChanges();
+            if (result>0)
+            {
+
+            }
+
+            return View(getirCategory);
+        }
+
+        [HttpPost]
+        public ActionResult CategoryDelete(int categoryId,string categoryName)
+        {
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            var getirCategory = db.Categories.Where(p => p.CategoryID == categoryId).FirstOrDefault();
+            db.Categories.Remove(getirCategory);
+
+            int result = db.SaveChanges();
+            if (result > 0)
+            {
+
+            }
+
+            return View(getirCategory);
+        }
     }
 }
